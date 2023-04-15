c65cef8c81bf4e3eebfad118796b263a074be2a1
Wesonga Bob <31077716+wesonga@users.noreply.github.com>
2023-04-15 18:11:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
-    $stripe = new \Stripe\StripeClient('sk_test_51Mk5JeLVQDdnavrA6V4UdNlUje66EmUuZgrO4rdd3JQKtDKgCrmH9PPWTHKnjBtB9O1KeWh55M7KLSOI9uw0PWvc003ojypyc9');
+    $stripe = new \Stripe\StripeClient('Your Stripe client key');
 
 // Use an existing Customer ID if this is a returning customer.
     $customer = $stripe->customers->create(
@@ -36,7 +36,7 @@ require 'vendor/autoload.php';
             'paymentIntent' => $paymentIntent->client_secret,
             'ephemeralKey' => $ephemeralKey->secret,
             'customer' => $customer->id,
-            'publishableKey' => 'pk_test_51Mk5JeLVQDdnavrAfTQHPytjqoj8dyRVBHDrrv6vm0AaA6T7FzjTBWf1auRG8eda00leDsPzBmDx1x0qrSsjwVqc005UYpyCBu'
+            'publishableKey' => 'Your stripe publishable key'
         ]
     );
     http_response_code(200);
